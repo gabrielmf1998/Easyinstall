780106dc32931273a48e2cb6bd62cf313e90f9f2
Gabriel Marques Ferrarezi <110578985+gabrielmf1998@users.noreply.github.com>
2025-08-16 15:40:13 -0300

    Add files via upload

--- pwsh7.ps1
+++ pwsh7.ps1
@@ -1,4 +1,15 @@
 #Vai instalar pwsh 7.5
-$installerPath = "$env:TEMP\PowerShell-7.5.2-win-x64.exe"
-Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.5.2/PowerShell-7.5.2-win-x64.exe" -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
+Write-Host "Power Shell 7.5" -ForegroundColor Magenta
+Write-Host "1 | Instalar"
+Write-Host "2 | Sair"
+$escolha = Read-Host "Digite uma opção "
+    switch ($escolha) {
+        "1" {
+            $installerPath = "$env:TEMP\PowerShell-7.5.2-win-x64.exe"
+            Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.5.2/PowerShell-7.5.2-win-x64.exe" -OutFile $installerPath
+            Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
+        }
+        "2" {
+            exit
+        }
+    }
