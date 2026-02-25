213b50f929bc15ac36b52ed8577d016a6bf6dc3e
Gabriel Marques Ferrarezi <110578985+gabrielmf1998@users.noreply.github.com>
2026-02-25 18:53:41 -0300

    Delete Easyinstall-Executor.ps1

--- Easyinstall-Executor.ps1
+++
@@ -1,6 +0,0 @@
-#Vai instalar chamar outro script em adm
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri "https://github.com/gabrielmf1998/Easyinstall/releases/download/Easyinstall-Executor/Easyinstall.ps1" -OutFile "$env:TEMP\Easyinstall.ps1"
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$env:TEMP\Easyinstall.ps1`"" -Verb RunAs
-exit
